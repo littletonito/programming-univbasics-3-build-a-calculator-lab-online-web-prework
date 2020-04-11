67230502bfcb527c2f5b37785c7c832411b86be9
Antonio Clavijo Cortes <littletonito@gmail.com>
2020-04-11 18:28:55 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -2,22 +2,22 @@ def addition(num1, num2)
   puts (num1 + num2)
 end
 
-def subtraction(num1, num2)
-  puts (num1 - num2)
-end
+# def subtraction(num1, num2)
+#   puts (num1 - num2)
+# end
 
-def division(num1, num2)
-  # puts (num1 / num2)
-end
+# def division(num1, num2)
+#   # puts (num1 / num2)
+# end
 
-def multiplication(num1, num2)
-  # puts (num1 * num2)
-end
+# def multiplication(num1, num2)
+#   # puts (num1 * num2)
+# end
 
-def modulo(num1, num2)
-  # puts (num1 % num2)
-end
+# def modulo(num1, num2)
+#   # puts (num1 % num2)
+# end
 
-def square_root(num)
-# puts (num)
-end
+# def square_root(num)
+# # puts (num)
+# end
